bf13ea1d738be8fcdd75129befdec2840d61a340
Santi <sdetord@lasalle.cat>
2018-04-09 12:38:01 +0200

    Added constructor to class Posicion.

--- main/java/Posicion.java
+++ main/java/Posicion.java
@@ -2,6 +2,11 @@ public class Posicion {
     private int x;
     private int y;
 
+    public Posicion(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public int getX() {
         return x;
     }
